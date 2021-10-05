48f9b06316a8ccc80bc7c3cec4e18fce0832ab8f
Vikraman Choudhury <git@vikraman.org>
2021-10-05 19:01:51 +0100

    Apply hlint fixes

--- tests/Test.hs
+++ tests/Test.hs
@@ -32,15 +32,15 @@ qcProps =
   testGroup
     "QuickCheck tests"
     [ QC.testProperty "Zero" $
-        (eval (Zero :: Exp)) == 0,
+        eval (Zero :: Exp) == 0,
       QC.testProperty "Succ" $
-        \x -> (eval (x :: Exp)) + 1 == ((eval (Succ (x))) :: Int),
+        \x -> eval (x :: Exp) + 1 == (eval (Succ x) :: Int),
       QC.testProperty "Addition" $
         \x y ->
-          (eval (x :: Exp)) + (eval (y :: Exp)) == (eval (Add x y) :: Int),
+          eval (x :: Exp) + eval (y :: Exp) == (eval (Add x y) :: Int),
       QC.testProperty "Multiplication" $
         \x y ->
-          (eval (x :: Exp)) * (eval (y :: Exp)) == (eval (Mul x y) :: Int)
+          eval (x :: Exp) * eval (y :: Exp) == (eval (Mul x y) :: Int)
     ]
 
 unitTests :: TestTree
@@ -48,9 +48,9 @@ unitTests =
   testGroup
     "Unit tests"
     [ testCase "Zero" $
-        eval (Zero) @?= 0,
+        eval Zero @?= 0,
       testCase "Succ" $
-        (eval (Succ (Succ (Succ (Zero))))) @?= ((1 + (eval (Succ (Succ (Zero))))) :: Int),
+        eval (Succ (Succ (Succ Zero))) @?= ((1 + eval (Succ (Succ Zero))) :: Int),
       testCase "Addition" $
-        (eval (Succ (Succ (Succ (Zero))))) + (eval (Succ (Succ (Zero)))) @?= (eval (Succ (Succ (Succ (Succ (Succ (Zero)))))) :: Int)
+        eval (Succ (Succ (Succ Zero))) + eval (Succ (Succ Zero)) @?= (eval (Succ (Succ (Succ (Succ (Succ Zero))))) :: Int)
     ]
