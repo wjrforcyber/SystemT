4437137d23755770a5b97a7b7db7a16b45670f2c
Vikraman Choudhury <git@vikraman.org>
2021-10-08 02:38:53 +0100

    Add parser tests

--- tests/Test.hs
+++ tests/Test.hs
@@ -6,37 +6,56 @@ module Main where
 import Common.Types
 import Lang.L1.Eval
 import Lang.L1.Syntax
+import qualified Prettyprinter as PP
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.QuickCheck as QC
+import qualified Text.Parsec as P
 
 main :: IO ()
 main = defaultMain tests
+
+parse :: String -> Either P.ParseError Exp
+parse = P.parse parseExp ""
+
+pp :: Exp -> String
+pp = show . PP.pretty
 
 tests :: TestTree
 tests = testGroup "Tests" [propertyTests, unitTests]
 
 propertyTests :: TestTree
-propertyTests = testGroup "Property tests" [qcProps]
+propertyTests = testGroup "Property tests" [evalProps, parserProps]
 
-qcProps :: TestTree
-qcProps =
+evalProps :: TestTree
+evalProps =
   testGroup
-    "QuickCheck tests"
-    [ QC.testProperty "Nat" $
+    "eval"
+    [ QC.testProperty "eval of Nat is itself" $
         \(x :: Nat) -> eval (ENat x) == x,
-      QC.testProperty "Addition" $
+      QC.testProperty "eval of Add is +" $
         \(x :: Exp) (y :: Exp) ->
           eval x + eval y == eval (EAdd x y),
-      QC.testProperty "Multiplication" $
+      QC.testProperty "eval of Mul is *" $
         \(x :: Exp) (y :: Exp) ->
           eval x * eval y == eval (EMul x y)
     ]
 
-unitTests :: TestTree
-unitTests =
+parserProps :: TestTree
+parserProps =
   testGroup
-    "Unit tests"
+    "parse/prettyprint"
+    [ QC.testProperty "parse . pp is identity" $
+        \(e :: Exp) -> (parse . pp) e == Right e
+    ]
+
+unitTests :: TestTree
+unitTests = testGroup "Unit tests" [evalUnitTests, parserUnitTests]
+
+evalUnitTests :: TestTree
+evalUnitTests =
+  testGroup
+    "eval"
     [ testCase "Zero" $
         eval (ENat Zero) @?= 0,
       testCase "Nat" $
@@ -45,4 +64,12 @@ unitTests =
         eval (EAdd (EMul (ENat 2) (ENat 3)) (ENat 3)) @?= 9,
       testCase "Multiplication" $
         eval (EMul (ENat 2) (ENat 3)) @?= 6
+    ]
+
+parserUnitTests :: TestTree
+parserUnitTests =
+  testGroup
+    "parser"
+    [ testCase "2 + 3 * 4" $
+        parse "2 + 3 * 4" @?= Right (EAdd (ENat 2) (EMul (ENat 3) (ENat 4)))
     ]
