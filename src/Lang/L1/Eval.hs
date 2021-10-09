fa4e0b4d5fb252fdff4e8b3230ad0779b1fdc7ed
wjrforcyber <wjrforcyber@163.com>
2021-10-09 13:01:43 +0100

    All tests passed

--- src/Lang/L1/Eval.hs
+++ src/Lang/L1/Eval.hs
@@ -14,6 +14,8 @@ eval (EMul e1 e2) = eval e1 * eval e2
 opt :: Exp -> Exp
 opt (ENat n) = ENat n
 
+opt (EAdd (ENat e1) (ENat e2)) = ENat (e1+e2)
+
 opt (EMul _ (ENat 0)) = ENat 0
 opt (EMul e1 (ENat(Succ(e2)))) = EAdd (opt (e1)) (opt (EMul (e1) (ENat e2)))
 
@@ -21,4 +23,7 @@ opt (EMul (ENat 0) _) = ENat 0
 opt (EMul (ENat(Succ(e1))) e2 ) = EAdd (opt (EMul (ENat e1) e2)) (opt (e2))  
 
 opt (EAdd e1 e2) = EAdd (opt(e1)) (opt(e2))
-opt (EMul e1 e2) = EMul (opt(e1)) (opt(e2))
+
+opt (EMul e1 e2) = opt (EMul (opt(e1)) (opt(e2)))
+
+
