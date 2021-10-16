401273e68f88bc1a36970b812d6ae0d92156b878
Vikraman Choudhury <git@vikraman.org>
2021-10-16 22:11:12 +0100

    Add template eval for L2

---
+++ src/Lang/L2/Eval.hs
@@ -0,0 +1,8 @@
+-- | Evaluator for L2
+
+module Lang.L2.Eval where
+
+import Lang.L2.Syntax
+
+eval :: Exp -> Maybe Val
+eval _ = Nothing
--- src/Lang/L2/Syntax.hs
+++ src/Lang/L2/Syntax.hs
@@ -4,9 +4,10 @@ module Lang.L2.Syntax where
 import Common.Types
 import Test.QuickCheck
 
-data Ty = TNat
-        | TBool
-        deriving (Eq, Show)
+data Ty
+  = TNat
+  | TBool
+  deriving (Eq, Show)
 
 data Exp
   = ENat Nat
@@ -16,22 +17,28 @@ data Exp
   | EIf Exp Exp Exp
   deriving (Eq, Show)
 
+data Val
+  = VNat Nat
+  | VBool Bool
+
 instance Arbitrary Ty where
-  arbitrary = oneof [ pure TNat, pure TBool ]
+  arbitrary = oneof [pure TNat, pure TBool]
 
 instance Arbitrary Exp where
   arbitrary = sized arbExp
 
 arbExp :: Int -> Gen Exp
 arbExp 0 =
-  oneof [ ENat <$> arbitrary `suchThat` \n -> n < 30
-        , EBool <$> arbitrary
-        ]
+  oneof
+    [ ENat <$> arbitrary `suchThat` \n -> n < 30,
+      EBool <$> arbitrary
+    ]
 arbExp n = do
   (Positive m) <- arbitrary
   let subExp = arbExp (n `div` (m + 1))
-  oneof [subExp
-        , EAdd <$> subExp <*> subExp
-        , EMul <$> subExp <*> subExp
-        , EIf  <$> subExp <*> subExp <*> subExp
-        ]
+  oneof
+    [ subExp,
+      EAdd <$> subExp <*> subExp,
+      EMul <$> subExp <*> subExp,
+      EIf <$> subExp <*> subExp <*> subExp
+    ]
