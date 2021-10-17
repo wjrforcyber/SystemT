357eaebf101106b39f2b469f5763fb4c41853cce
wjrforcyber <wjrforcyber@163.com>
2021-10-17 11:25:55 +0100

    All tests passed

--- src/Lang/L2/Typecheck.hs
+++ src/Lang/L2/Typecheck.hs
@@ -2,7 +2,8 @@ module Lang.L2.Typecheck where
 
 import Lang.L2.Syntax
 
---checker
+--checker
+
 check :: Exp->Ty->Bool
 
 check (ENat _) TNat = True
@@ -11,20 +12,21 @@ check (EBool _) TBool = True
 check (EAdd e1 e2) TNat = check e1 TNat && check e2 TNat
 check (EMul e1 e2) TNat = check e1 TNat && check e2 TNat
 
-check (EIf e1 e2 e3) TNat = check e1 TBool && (check e2 TNat || check e3 TNat)
-check (EIf e1 e2 e3) TBool = check e1 TBool && (check e2 TBool || check e3 TBool)
+check (EIf e1 e2 e3) TNat =  if check e1 TBool then if e1 == (EBool True) then check e2 TNat else check e3 TNat else False
+check (EIf e1 e2 e3) TBool = if check e1 TBool then if e1 == (EBool True) then check e2 TBool else check e3 TBool else False
 
 check _ _ = False
 
 
---infer
+--infer
+
 infer :: Exp->Maybe Ty
+
 infer (ENat _) = Just TNat
 infer (EBool _) = Just TBool
-infer (EAdd e1 e2) = Just TNat
-infer (EMul e1 e2) = Just TNat
 
-infer (EIf (EBool True) e2 e3) = infer e2
-infer (EIf (EBool False) e2 e3) = infer e3
-infer (EIf e1 e2 e3)  = infer (EIf (EBool (check e1 TBool)) e2 e3)
+infer (EAdd e1 e2) = if (infer e1 == Just TNat) && (infer e2 == Just TNat) then Just TNat else Nothing
+infer (EMul e1 e2) = if (infer e1 == Just TNat) && (infer e2 == Just TNat) then Just TNat else Nothing
+infer (EIf e1 e2 e3) = if check e1 TBool then if e1 == (EBool True) then infer e2 else infer e3 else Nothing
+
 infer _ = Nothing
